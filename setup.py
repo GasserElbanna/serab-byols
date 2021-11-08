d5783a87f5ef0bb863712c17b5a6a39919fd2c23
GasserElbanna <gasser.elbanna@epfl.ch>
2021-11-08 08:12:27 +0100

    :heavy_plus_sign: update setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "Source Code": "",
     },
     packages=find_packages(exclude=("tests",)),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
         "librosa==0.8.1",
         "numba==0.48",
